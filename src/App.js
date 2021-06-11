91ed883adb9005532ccf0f5851de82cb17c930ec
pebueno <pedroivobu@gmail.com>
2021-06-11 12:38:45 -0300

    Estruturando o projeto com React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,39 +1,28 @@
-import axios from "axios";
 import "./App.css";
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 
-const api_key = process.env.REACT_APP_API_KEY;
-const BASE_URL = "https://api.themoviedb.org/3";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
-function App() {
-  const [data, setData] = useState([]);
+//Pages
+import MainPage from "./pages";
+import NotFound from "./pages/404";
 
-  const api = axios.create({ baseURL: BASE_URL });
-
-  const getUpcoming = api.get("movie/upcoming", {
-    params: { api_key },
-  });
-
-  useEffect(() => {}, []);
-
-  getUpcoming.then((response) => {
-    setData(response.data.results);
-  });
-
-  const getImage = (path) => `https://image.tmdb.org/t/p/w500/${path}`;
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        {data.map((movie) => (
-          <div>
-            <img src={getImage(movie.poster_path)} alt={movie.original_title} />
-            <p>{movie.original_title}</p>
-          </div>
-        ))}
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/404" component={NotFound} />
+          <Redirect to="/404" />
+        </Switch>
+      </Router>
+    );
+  }
 }
-
 export default App;
