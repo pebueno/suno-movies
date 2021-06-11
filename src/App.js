7355ffaa135a0d18f9505d49a32f746624e55818
pebueno <pedroivobu@gmail.com>
2021-06-11 12:11:41 -0300

    Instalando Axios e usando API do TMDB

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
+import React, { useEffect, useState } from "react";
+
+const api_key = process.env.REACT_APP_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const api = axios.create({ baseURL: BASE_URL });
+
+  const getUpcoming = api.get("movie/upcoming", {
+    params: { api_key },
+  });
+
+  useEffect(() => {}, []);
+
+  getUpcoming.then((response) => {
+    setData(response.data.results);
+  });
+
+  const getImage = (path) => `https://image.tmdb.org/t/p/w500/${path}`;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {data.map((movie) => (
+          <div>
+            <img src={getImage(movie.poster_path)} alt={movie.original_title} />
+            <p>{movie.original_title}</p>
+          </div>
+        ))}
       </header>
     </div>
   );
