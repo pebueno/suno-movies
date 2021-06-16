f91bf52853f248d4304b6db670d72e6ead3ad168
pebueno <pedroivobu@gmail.com>
2021-06-16 20:50:30 -0300

    Mudando className com dois botoes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -11,11 +11,19 @@ import { ReactComponent as Ellipse } from "../../images/ellipse.svg";
 import api from "../services/Api";
 import Carousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
+
 const api_key = process.env.REACT_APP_API_KEY;
 
 export default function Home() {
   //Filmes mais recentes no banco de dados
   const [data, setData] = useState([]);
+  // const [changeClass, setChangeClass] = useState("false");
+
+  // const ToggleClass = () => {
+  //   setChangeClass(!changeClass);
+  // };
+
+  const [className, setClassName] = useState("grid");
 
   const getUpcoming = api.get("movie/upcoming", {
     params: { api_key },
@@ -77,14 +85,64 @@ export default function Home() {
           </div>
         </div>
         <div className="catalogo-container">
-          <select className="catalogo-filtro">
-            <option>por gênero</option>
-          </select>
-          <button className="catalogo-populares">mais populares</button>
-          <select className="catalogo-exibicao">
-            <option>em grid</option>
-            <option>em lista</option>
-          </select>
+          <div className="catalogo-escolhas">
+            <select className="catalogo-filtro">
+              <option>por gênero</option>
+            </select>
+            <button className="catalogo-populares">mais populares</button>
+            <select className="catalogo-exibicao">
+              <option
+                value="grid"
+                // onClick={ToggleClass}
+              >
+                em grid
+              </option>
+              <option value="list">em lista</option>
+            </select>
+            <button
+              className="view-btn list-view"
+              title="List View"
+              onClick={() => setClassName("list")}
+            >
+              List View
+            </button>
+            <button
+              className="view-btn list-view"
+              title="Grid View"
+              onClick={() => setClassName("grid")}
+            >
+              Grid View
+            </button>
+          </div>
+          <div
+            // className={changeClass ? "grid" : "list"}
+            className={className}
+          >
+            {data.map((movie) => (
+              <Link className="movie-card">
+                <table>
+                  <tr>
+                    <td>
+                      <img
+                        className="movie-image"
+                        src={getImage(movie.poster_path)}
+                        alt={movie.title}
+                      />{" "}
+                    </td>
+                    <td className="movie-data">
+                      <p className="movie-title">{movie.title}</p>
+                      <p className="movie-genre">{movie.genres_name}</p>
+                      <p className="movie-rate">
+                        <Star className="star" />
+                        {movie.vote_average}
+                      </p>
+                      <p className="movie-overview">{movie.overview}</p>
+                    </td>
+                  </tr>
+                </table>
+              </Link>
+            ))}
+          </div>
         </div>
       </section>
     </>
